e9e6dc8a58692ddecb8cc5f87a1c881295445aca
Haukur Kristinsson <haukur@hauxi.is>
2014-12-05 15:25:04 +0000

    main classes are now working with able to list channels and get messages for channels.

--- pyslaquery/__init__.py
+++ pyslaquery/__init__.py
@@ -1,5 +1,6 @@
 import logging
 from pyslaquery.utils import RequestsHelper
+from pyslaquery.exception import SlackAPIError
 
 
 class SlackBase(object):
@@ -10,34 +11,31 @@ class SlackBase(object):
 
 
 class SlackQueryClient(SlackBase):
-
     def __init__(self, token):
         super(SlackQueryClient, self).__init__(token)
 
-    def get_messages_for_channel(self, channel, text, **params):
-        """search.messages
+    def get_messages_for_channel(self, req_channel, **params):
+        """channels.history
         """
-        method = 'chat.postMessage'
+        method = 'channels.history'
+
+        channels = self.get_channel_list()
+
+        for channel in channels:
+            if channel["name"] == req_channel:
+                params.update({
+                    'token': self.token,
+                    'channel': channel["id"],
+                })
+                return self.HELPER.create_post_request(method, params).json()
+
+        raise SlackAPIError("Could not find the channel specified.")
+
+    def get_channel_list(self, **params):
+        """channels.list
+        """
+        method = 'channels.list'
         params.update({
-            'channel': channel,
-            'text': text,
+            'token': self.token
         })
-        return self.HELPER.create_post_request(method, params).json()
-
-
-class SlackHandler(logging.Handler):
-    """A logging handler that posts messages to a Slack channel!
-    References:
-    http://docs.python.org/2/library/logging.html#handler-objects
-    """
-    def __init__(self, token, channel, **kwargs):
-        super(SlackHandler, self).__init__()
-        self.client = SlackQueryClient(token)
-        self.channel = channel
-        self._kwargs = kwargs
-
-    def emit(self, record):
-        message = self.format(record)
-        self.client.chat_post_message(self.channel,
-                                      message,
-                                      **self._kwargs)
+        return self.HELPER.create_get_request(method, params).json()["channels"]

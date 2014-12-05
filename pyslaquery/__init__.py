05430ab01e7766758fa8bb2bd7086483f6ed7b24
Haukur Kristinsson <haukur@hauxi.is>
2014-12-05 17:02:56 +0000

    added more search options when searching for messages.

--- pyslaquery/__init__.py
+++ pyslaquery/__init__.py
@@ -1,41 +1,66 @@
-import logging
-from pyslaquery.utils import RequestsHelper
+from pyslaquery.utils import find_channel_id, filter_out_message_subtypes
+from pyslaquery.connection import SlackAPIConnection
 from pyslaquery.exception import SlackAPIError
 
 
 class SlackBase(object):
-    HELPER = RequestsHelper("https://slack.com/api/")
+    _slackconnection = None
 
     def __init__(self, token):
-        self.token = token
+        self._slackconnection = SlackAPIConnection("https://slack.com/api", token)
 
 
 class SlackQueryClient(SlackBase):
     def __init__(self, token):
         super(SlackQueryClient, self).__init__(token)
 
-    def get_messages_for_channel(self, req_channel, **params):
-        """channels.history
+    def get_messages_for_channel(self, req_channel, no_subtypes=True, count=10, **params):
+        """
+        Fetches history of messages and events from a channel.
+        Method: channels.history
+
+        :param req_channel: Channel to get messages from.
+        :param no_subtypes: Show only plain messages, no messages with subtypes.
+        :param count: Limit number of showned messages
+        :param params: extra parameters to send with the request to the Slack API.
+        return: dict object from the Slack API.
         """
         method = 'channels.history'
 
-        channels = self.get_channel_list()
+        channel_id = find_channel_id(self.get_channel_list(), req_channel)
 
-        for channel in channels:
-            if channel["name"] == req_channel:
-                params.update({
-                    'token': self.token,
-                    'channel': channel["id"],
-                })
-                return self.HELPER.create_post_request(method, params).json()
+        if channel_id is not None:
+            params.update({
+                'channel': channel_id,
+                'count': count
+            })
+            channel_response = self._slackconnection.create_post_request(method, params).json()
+            if no_subtypes is True:
+                channel_response["messages"] = filter_out_message_subtypes(channel_response["messages"])
+            return channel_response
 
         raise SlackAPIError("Could not find the channel specified.")
 
     def get_channel_list(self, **params):
-        """channels.list
+        """
+        Lists all channels in a Slack team.
+        Method: channels.list
+
+        return: dict object from the Slack API.
         """
         method = 'channels.list'
+        return self._slackconnection.create_get_request(method, params).json()["channels"]
+
+    def get_channel_info(self, req_channel, **params):
+        """
+        Gets information about a channel.
+        Method: channels.info
+
+        :param params: extra parameters to send with the request to the Slack API.
+        :return: dict object from the Slack API.
+        """
+        method = 'channels.info'
         params.update({
-            'token': self.token
+            'channel': req_channel
         })
-        return self.HELPER.create_get_request(method, params).json()["channels"]
+        return self._slackconnection.create_get_request(method, params).json()["channels"]

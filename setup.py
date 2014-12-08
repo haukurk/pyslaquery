876bad0790a15b7af9dbbd349e6b8a5d1e0313e5
Haukur Kristinsson <haukur@hauxi.is>
2014-12-08 10:43:30 +0000

    bumped to v0.2.0

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 
 setup(
     name="pyslaquery",
-    version="0.1.8",
+    version="0.2.0",
     description="A really simple wrapper around Slack API, for querying information from Slack channels and users.",
     author="@haukurk",
     author_email="haukur@hauxi.is",
     packages=["pyslaquery"],
     install_requires=["requests"],
     url='https://github.com/haukurk/pyslaquery',
-    download_url='https://github.com/haukurk/pyslaquery/releases/tag/v0.1.8',
+    download_url='https://github.com/haukurk/pyslaquery/releases/tag/v0.2.0',
     keywords=['slack', 'channels', 'query'],
     classifiers=[]
 )
